e14f822dfa7c502a6f21df4e8913a74a50e5a6e6
Graham Carling <graham@blendlabs.com>
2017-01-12 14:39:29 -0800

    added objective-c code

--- ios/RNScreenshotDetector.h
+++ ios/RNScreenshotDetector.h
@@ -1,7 +1,15 @@
+//
+//  RNScreenshotDetector.h
+//
+//  Created by Graham Carling on 1/11/17.
+//
 
+#import "RCTEventEmitter.h"
 #import "RCTBridgeModule.h"
 
-@interface RNScreenshotDetector : NSObject <RCTBridgeModule>
+@interface RNScreenshotDetector : RCTEventEmitter <RCTBridgeModule>
+
+- (void)setupAndListen:(RCTBridge*)bridge;
+- (void)screenshotDetected:(NSNotification*)notification;
 
 @end
-  
--- ios/RNScreenshotDetector.m
+++ ios/RNScreenshotDetector.m
@@ -1,13 +1,36 @@
+//
+//  RNScreenshotDetector.m
+//
+//  Created by Graham Carling on 1/11/17.
+//
 
 #import "RNScreenshotDetector.h"
+#import "RCTBridge.h"
+#import "RCTEventDispatcher.h"
 
 @implementation RNScreenshotDetector
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
+RCT_EXPORT_MODULE();
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"ScreenshotTaken"];
 }
-RCT_EXPORT_MODULE()
+
+- (void)setupAndListen:(RCTBridge*)bridge {
+    // First set up native bridge
+    [self setBridge:bridge];
+    // Now set up handler to detect if user takes a screenshot
+    NSOperationQueue *mainQueue = [NSOperationQueue mainQueue];
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationUserDidTakeScreenshotNotification
+                                                      object:nil
+                                                       queue:mainQueue
+                                                  usingBlock:^(NSNotification *notification) {
+                                                      [self screenshotDetected:notification];
+                                                  }];
+}
+
+- (void)screenshotDetected:(NSNotification *)notification {
+    [self.bridge.eventDispatcher sendAppEventWithName:@"ScreenshotTaken" body:notification];
+}
 
 @end
-  
